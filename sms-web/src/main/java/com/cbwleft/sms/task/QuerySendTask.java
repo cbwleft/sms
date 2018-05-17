a757d17247ca9d6a5003ffed51e5d3479a543abb
cbwleft <cbwleft@foxmail.com>
2018-05-17 16:51:57 +0800

    发送短信后10秒开始检查发送状态

--- sms-web/src/main/java/com/cbwleft/sms/task/QuerySendTask.java
+++ sms-web/src/main/java/com/cbwleft/sms/task/QuerySendTask.java
@@ -1,6 +1,8 @@
 package com.cbwleft.sms.task;
 
 import java.util.List;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -13,19 +15,31 @@ import com.cbwleft.sms.service.IMessageService;
 
 @Component
 public class QuerySendTask {
-	
+
 	private Logger logger = LoggerFactory.getLogger(getClass());
-	
+
+	@Autowired
+	private ScheduledExecutorService scheduledExecutor;
+
 	@Autowired
 	private IMessageService messageService;
-	
-	@Scheduled(fixedDelay = 5*60*1000)
+
+	@Scheduled(fixedDelay = 5 * 60 * 1000)
 	public void querySend() {
 		logger.debug("开始查询短信发送结果");
 		List<Message> list = messageService.querySendingMessages();
 		list.forEach(message -> {
 			messageService.queryAndUpdateSendStatus(message);
 		});
+	}
+
+	public void scheduledQuerySend(Message message) {
+		logger.debug("等待10秒");
+		scheduledExecutor.schedule(() -> {
+			logger.debug("执行短信查询任务");
+			messageService.queryAndUpdateSendStatus(message);
+		}, 10, TimeUnit.SECONDS);
+
 	}
 
 }
